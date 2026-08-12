3819102eb366f15800de04ac2621da9bc81cc4d4
AlifAqilAzizi <alifaqila142@gmail.com>
2026-08-12 19:20:32 +0700

    instalasi react menggunakan vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Header from './Komponen/Header.jsx'
+
+function App() {
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Header />
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
+
+export default App
